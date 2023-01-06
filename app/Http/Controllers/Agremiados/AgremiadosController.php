13221e6e34faed5614876282328cfdad7ff7bff9
Lino Puma Ticona <nearlino20@gmail.com>
2023-01-06 00:24:04 -0500

    Elimnar pago corregido ?

--- app/Http/Controllers/Agremiados/AgremiadosController.php
+++ app/Http/Controllers/Agremiados/AgremiadosController.php
@@ -139,7 +139,8 @@ class AgremiadosController extends Controller
     {
         try {
             DB::transaction(function () use ($idpago) {
-                Pago::where('idpago', $idpago)->delete();
+               //Pago::where('idpago', $idpago)->delete();
+                DB::delete("DELETE FROM pago_detalle WHERE idpago = $idpago;");
                 DB::delete("DELETE FROM pago_detalle WHERE idpago = $idpago;");
                 DB::delete("DELETE FROM pago_voucher WHERE idpago = $idpago;");
             });
