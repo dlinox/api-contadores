98b1ac243305065bc05c739f705c22d245c0420a
Lino Puma Ticona <nearlino20@gmail.com>
2023-01-15 21:25:37 -0500

    asdad

--- app/Models/Pago.php
+++ app/Models/Pago.php
@@ -48,10 +48,10 @@ class Pago extends Model
                     'concepto' => $pago->desconcepto,
                     'precio' => $pago->total,
 
-                    'numvoucher' => $pago->numvoucher ? $pago->numvoucher : '' ,
+                    'numvoucher' => $pago->numvoucher ? $pago->numvoucher : '',
                     'importe' => $pago->importe ? $pago->importe : 0.0,
                     'fecha_pago' => $pago->fecha  ? $pago->fecha : '',
-                    'imagen' => "uploads/{$pago->imagen}",
+                    'imagen' => "uploads/{$pago->imagen}" ??  "uploads/{$pago->imagen}",
                 ];
             });
     }
